ebdcf53031bec56f6a0b37cd7b84edbf91173e96
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2025-05-15 16:58:30 +0530

    added backend logic

---
+++ helper/sendVerificationmail.ts
@@ -0,0 +1,42 @@
+import { ApiResponse } from "@/lib/apiresponse";
+
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export async function sendVerificationEmail(email: string,
+  name: string,
+  verifyCode: string): Promise<ApiResponse> {
+
+  const mailOptions = {
+    from: `"Starter" <${process.env.EMAIL}>`,
+    to: email,
+    subject: "saasstarter- Verify your email",
+    text: `Hello ${name}, Your verification code is ${verifyCode}`,
+    html: `<b>Hello ${name}, Your verification code is ${verifyCode}</b>`,
+  }
+
+  try {
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(mailOptions, (err: Error | null, info: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      });
+    });
+
+    return { success: true, message: 'Verification email sent successfully.' };
+  } catch (error) {
+    return { success: false, message: 'Failed to send verification email.' };
+  }
+}
